cf411f0c29ab72527a7c7848bb17133ee3a855c7
Mouad Oumous <mouadoumous@gmail.com>
2025-12-14 14:33:55 +0100

    set up a toolbar in main activity

--- app/src/main/java/io/droidevs/counterapp/MainActivity.kt
+++ app/src/main/java/io/droidevs/counterapp/MainActivity.kt
@@ -1,14 +1,20 @@
 package io.droidevs.counterapp
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 import io.droidevs.counterapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , Toolbar.OnMenuItemClickListener{
     var binding : ActivityMainBinding? = null
+
+    private var toolbar : MaterialToolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +29,28 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        toolbar = binding!!.mainToolbar
+        toolbar?.setNavigationOnClickListener { v: View? ->
+            TODO("open a drawer")
+        }
+        toolbar?.setOnMenuItemClickListener(this)
+
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_edit -> {
+                TODO("edit counter")
+            }
+            R.id.menu_delete -> {
+                TODO("delete counter")
+            }
+            R.id.menu_reset -> {
+                TODO("reset counter")
+            }
+        }
+        return true
     }
 }
