aa6a48db89d71edf08fd347aa5c850dabb2b7bfc
Mouad Oumous <mouadoumous@gmail.com>
2025-12-16 18:22:43 +0100

    implement the logic in the edit page

--- app/src/main/java/io/droidevs/counterapp/ui/CounterEditFragment.kt
+++ app/src/main/java/io/droidevs/counterapp/ui/CounterEditFragment.kt
@@ -1,19 +1,28 @@
 package io.droidevs.counterapp.ui
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import io.droidevs.counterapp.R
 import io.droidevs.counterapp.databinding.FragmentCounterEditBinding
+import io.droidevs.counterapp.model.Counter
 
 class CounterEditFragment : Fragment() {
 
     lateinit var binding : FragmentCounterEditBinding
 
+    private lateinit var counter : CounterSnapshot
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        counter = requireArguments()
+            .getParcelable<CounterSnapshotParcelable>(ARG_COUNTER)!!
+            .toUiModel()
     }
 
     override fun onCreateView(
@@ -22,12 +31,26 @@ class CounterEditFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentCounterEditBinding.inflate(inflater, container, false)
+
+        with(binding) {
+            etName.setText(counter.name)
+            etCurrentCount.setText(counter.currentCount.toString())
+            switchCanIncrease.isChecked = counter.canIncrease
+            switchCanDecrease.isChecked = counter.canDecrease
+
+            tvCreatedAt.text = "Created at: ${counter.createdAt}"
+            tvLastUpdatedAt.text = "Last updated: ${counter.lastUpdatedAt}"
+        }
         return binding.root
     }
 
     companion object {
+
+        private const val ARG_COUNTER = "counter"
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CounterEditFragment()
+        fun newInstance(counter: CounterSnapshot) =
+            CounterEditFragment().apply {
+                arguments = bundleOf(ARG_COUNTER to counter.toParcelable())
+            }
     }
 }
