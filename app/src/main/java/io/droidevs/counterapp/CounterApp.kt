9328910d241d268aa04b6622f747c8c71df90d9b
Mouad <mouadoumous@gmail.com>
2026-01-13 16:32:31 +0100

    fix app

--- app/src/main/java/io/droidevs/counterapp/CounterApp.kt
+++ app/src/main/java/io/droidevs/counterapp/CounterApp.kt
@@ -5,15 +5,15 @@ import androidx.preference.PreferenceManager
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
+import dagger.hilt.android.HiltAndroidApp
 import io.droidevs.counterapp.data.AppDatabase
 import io.droidevs.counterapp.internal.scheduleSystemCounterSync
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import io.droidevs.counterapp.domain.usecases.category.*
 import io.droidevs.counterapp.domain.usecases.counters.*
 
+
+@HiltAndroidApp
 class CounterApp : Application() {
 
     @OptIn(DelicateCoroutinesApi::class)
