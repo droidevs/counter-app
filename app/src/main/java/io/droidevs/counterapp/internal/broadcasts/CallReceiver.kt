f001119dfa5288a9cd0914e38f3be2a8661cbcd1
mouadoumous <mouadoumous@gmail.com>
2026-01-13 10:58:18 +0000

    fix

--- app/src/main/java/io/droidevs/counterapp/internal/broadcasts/CallReceiver.kt
+++ app/src/main/java/io/droidevs/counterapp/internal/broadcasts/CallReceiver.kt
@@ -4,27 +4,29 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.telephony.TelephonyManager
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import dagger.hilt.android.AndroidEntryPoint
-import io.droidevs.counterapp.domain.repository.CounterRepository
 import io.droidevs.counterapp.domain.system.SystemCounterType
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import javax.inject.Inject
+import io.droidevs.counterapp.internal.worker.SystemEventWorker
 
 @AndroidEntryPoint
 class CallReceiver : BroadcastReceiver() {
-
-    @Inject
-    lateinit var repository: CounterRepository
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == TelephonyManager.ACTION_PHONE_STATE_CHANGED) {
             val state = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
             if (state == TelephonyManager.EXTRA_STATE_RINGING) {
-                CoroutineScope(Dispatchers.IO).launch {
-                    repository.incrementSystemCounter(SystemCounterType.CALLS_RECEIVED.name, 1)
-                }
+                val work = OneTimeWorkRequestBuilder<SystemEventWorker>()
+                    .setInputData(
+                        workDataOf(
+                            SystemEventWorker.COUNTER_KEY to SystemCounterType.CALLS_RECEIVED.key
+                        )
+                    )
+                    .build()
+
+                WorkManager.getInstance(context).enqueue(work)
             }
         }
     }
