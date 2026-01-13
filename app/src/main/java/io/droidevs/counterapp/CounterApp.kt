f1f466a7e7b50c8b361d9ce7b77b62ad4b85a379
Mouad <mouadoumous@gmail.com>
2026-01-13 15:36:12 +0100

    fix app

--- app/src/main/java/io/droidevs/counterapp/CounterApp.kt
+++ app/src/main/java/io/droidevs/counterapp/CounterApp.kt
@@ -16,28 +16,14 @@ import io.droidevs.counterapp.domain.usecases.counters.*
 
 class CounterApp : Application() {
 
-    lateinit var database: AppDatabase
-        private set
-
-
-
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate() {
         super.onCreate()
 
-        database = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java,
-            "counter_db"
-        ).build()
-
-
-        // todo: register system counters receivers
         scheduleSystemCounterSync(this)
     }
 
     override fun onTerminate() {
         super.onTerminate()
-        // unregister recievers
     }
 }
